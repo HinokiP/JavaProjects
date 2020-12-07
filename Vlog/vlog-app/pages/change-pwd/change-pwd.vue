dc25bdb4b902e9e8308473d31709886f87cf1e53
HinokiP <17448769@qq.com>
2020-12-07 22:33:58 +0800

    个人信息修改+修改密码校验

--- Vlog/vlog-app/pages/change-pwd/change-pwd.vue
+++ Vlog/vlog-app/pages/change-pwd/change-pwd.vue
@@ -1,11 +1,14 @@
 <template>
 	<view>
-		<input type="text" placeholder="输入新密码" class="p-3" />
-		<input type="text" placeholder="输入确认密码" class="p-3" />
+		<input type="text" placeholder="输入新密码" class="p-3 uni-input" v-model="newpassword" />
+		<input type="text" placeholder="输入确认密码" class="p-3 uni-input" v-model="renewpassword" />
 		<view class="py-2 px-3">
 			<button class="bg-main text-white mt-2"
 				 style="border-radius: 50rpx; border: 0;"
-				 type="primary">
+				 type="primary"
+				 :disabled="disabled"
+				 :class="disabled ? 'bg-main-disabled' : ''"
+				 @click="submit">
 				 设置
 			</button>
 		</view>
@@ -13,14 +16,39 @@
 </template>
 
 <script>
+	import { mapState } from 'vuex';
 	export default {
 		data() {
 			return {
-				
+				newpassword:'',
+				renewpassword:'',
+			};
+		},
+		computed: {
+			...mapState({
+				user: state => state.user
+			}),
+			disabled() {
+				return this.newpassword == '' || this.renewpassword == '';
 			}
 		},
 		methods: {
-			
+			//验证
+			check(){
+				if(this.newpassword !== this.renewpassword) {
+					uni.showToast({
+						title: '两次密码不一致',
+						icon: 'none'
+					});
+					return false;
+				}
+				return true;
+			},
+			submit() {
+				if(!this.check()) {
+					return;
+				}
+			}
 		}
 	}
 </script>
